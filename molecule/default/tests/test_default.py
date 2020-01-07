070ce90dee7b207a43dbf21436f3632f2c805782
tangelov <yo@tangelov.me>
2020-01-07 19:31:27 +0100

    Adding support in molecule tests for GPG encrypted files

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -10,7 +10,7 @@ def test_backupninja_dump_exists(host):
     """Checking if the file generated in default test last step exists"""
     with host.sudo():
         bckdate = datetime.datetime.utcnow().strftime("%Y.%m.%d")
-        bckpath = '/def_test/' + 'full-test' + '-' + bckdate + '.tgz'
+        bckpath = '/def_test/' + 'full-test' + '-' + bckdate + '.tgz.gpg'
 
         f = host.file(bckpath)
         assert f.exists
